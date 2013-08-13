9bc3e773b180ccb37c14722d32ec79373dba252d
Blurredbits <mmorris00@mac.com>
2013-08-13 08:58:15 -0600

    Base application with Bootstrap & Static Pages

---
+++ README.md
@@ -0,0 +1,12 @@
+### Job Application Tracker
+
+Application to help job-seekers keep track of where they have applied and the responses garnered from each application.
+Also supports entry of specific job descriptions, cover letters & resumes sent with each application.
+
+### Installation
+
+TBD
+
+### About
+
+Developed by Mark J. Morris, 2013 <@blurredbits>
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppTracker::Application.config.secret_token = '90b244be9d833fb986845a04096f41eb9fd1bb92ce8688c981c4d465a52bdce1a4bf94c0b165d05c2c1ff068a0ad1fac8d641d1fc4a162a51132bd4dfc72b22d'
